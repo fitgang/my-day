17b2d8fa838fd9e96a901d0d54e62afa6169e4a0
Ayush Gupta <guptaayush06348@gmail.com>
2022-05-24 22:22:01 +0530

    initialising redux

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Task from '../comp/Task'
 import InpustTask from '../comp/InputTask'
 import { useState } from 'react'
+import { validateData } from '../public/dataFunctions';
 
 export default function Home() {
   const [viewInputForm, setViewOfInputForm] = useState(false);
@@ -36,12 +37,30 @@ export default function Home() {
     // Raise warning if errors found
   }
 
-  function handleInputChange() {
+  function handleInputChange(e) {
+    const inputElem = e.target;
     // If the input has focus
+    if (document.activeElement === inputElem){
       // then validate data 
-        // If errors found, raise warning
-        // Else do nothing
+      const errors = validateInput(inputElem);
+      // If errors found, raise warning
+      if (errors.length !== 0) {
+        showErrorToUser()
+      }
+      // Else do nothing
+    }
     // Else Do nothing
+  }
+
+  function validateInput(inputElem) {
+    if (inputElem.type.includes("text")) {
+      return validateData(inputElem.value, "text")
+    }
+    return validateData(inputElem.value, "datetime")
+  }
+
+  function showErrorToUser() {
+
   }
 
   function hideInputForm() {
---
+++ public/dataFunctions.js
@@ -0,0 +1,8 @@
+export function validateData(data, type) {
+  // If data is empty, then return this error
+  if (data.length === 0) {
+    return ["fill this field."]
+  }
+
+  // Check the data according a/c its type
+}
