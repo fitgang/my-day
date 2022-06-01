a4a456c29216c69abb53088a21160faef93e0fc3
Ayush Gupta <guptaayush06348@gmail.com>
2022-06-01 23:07:03 +0530

    configured material-ui, and used it to create 1 component

--- comp/Task.jsx
+++ comp/Task.jsx
@@ -1,27 +1,82 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 export default function Task(props) {
-  const {heading, description, from, to} = props.task;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const { heading, description, from, to } = props.task;
 
   return (
-    <li className="task">
-
-      <section className="details">
-        <h2 className="heading">{heading}</h2>
-        <p className="description">{description}</p>
-      </section>
-
-      <div className="duration">
-        <div className="from">{from}</div>
-        <div className="to">{to}</div>
-      </div>
-
-      <div className="actions">
-        <div className="done"></div>
-        <div className="others">
-          <div className="delete"></div>
-          <div className="edit"></div>
-        </div>
-      </div>
-
-    </li>
-  )
+    <Card component="li">
+      <CardContent>
+        <Typography variant="h5" component="div" color="text.secondary">
+          {heading}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Typography paragraph>
+          Details -
+        </Typography>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            {description}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
+
+{/* <li className="task">
+
+<section className="details">
+  <h2 className="heading">{heading}</h2>
+  <p className="description">{description}</p>
+</section>
+
+<div className="duration">
+  <div className="from">{from}</div>
+  <div className="to">{to}</div>
+</div>
+
+<div className="actions">
+  <div className="done"></div>
+  <div className="others">
+    <div className="delete"></div>
+    <div className="edit"></div>
+  </div>
+</div>
+
+</li> */}
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,38 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../styles/theme';
+import createEmotionCache from '../js/createEmotionCache';
 import { wrapper } from '../store/store';
 import "../styles/global.css";
 
-const MyApp = ({ Component, pageProps }) => {
-  return  <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
 };
 
 export default wrapper.withRedux(MyApp);
